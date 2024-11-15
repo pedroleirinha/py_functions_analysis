2d29ec69b67e5c828ae727eedbe011d4743ccce2
Pedro <oleirinha.pedro@gmail.com>
2024-11-15 23:42:29 +0000

    refactor the code to use a Function class to better organize the code

--- function_analysis.py
+++ function_analysis.py
@@ -1,106 +1,31 @@
-import math
 import matplotlib.pyplot as plt
-import numpy as np
 from function_class import Function
 
 # coeff = [1, 5, -10]
 # print(f"roots: {np.polynomial.polynomial.polyroots(coeff)}")
-
-def plotFunction(function, showZeros, func_name_label = "f", plotColor = "blue"):
-    
-    plt.xlim(-5, 10)
-    plt.ylim(-5, 10)
-    
-    plt.plot(function.xpoints, function.ypoints, color=plotColor, label=f"{func_name_label}(x)={showFunction(func)}")
-    plt.legend(fontsize=14)
-    
-    # zerosX = findZeros(a,b,c)
-    
-    if showZeros:
-        print(f"The zeroes of the function are: {function.zerosX}")
-        zerosY = np.array(np.zeros(len(function.zerosX)))
-        
-        plt.plot(list(function.zerosX), zerosY, 'o', color=plotColor)
-
-
-
-def toSuperscript(number):
-    superscripts = {
-        '0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴',
-        '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹',
-        '+': '⁺', '-': '⁻', '=': '⁼', '(': '⁽', ')': '⁾'
-    }
-    return ''.join(superscripts.get(char, char) for char in str(number))
-        
-def showFunction(function):
-    degree = len(function.coeffArray) - 1
-    
-    text = ""
-    for coeff in function.coeffArray:
-        if coeff == 0:  # Skip zero coefficients
-            degree -= 1
-            continue
-        
-        if text:
-            text += "+" if coeff > 0 else "-"
-            
-        text += f"{abs(coeff)}"
-                
-        if degree == 1: text += "x"
-        if degree > 1:
-            text += f"x{toSuperscript(degree)}"
-        
-        degree-=1
-        
-    return text
-
-
-def findZeros(a, b, c):
-    discriminant = b**2-(4*a*c) # discriminant
-    
-    zeros = np.array([])
-    if discriminant < 0:
-        print ("This equation has no real solution")
-    elif discriminant == 0:
-        x = (-b+math.sqrt(discriminant))/(2*a)
-        zeros = np.append(zeros, x)
-        print ("This equation has one solutions: ", x)
-    else:
-        x1 = ((-b)+math.sqrt(discriminant))/(2*a)
-        x2 = ((-b)-math.sqrt(discriminant))/(2*a)
-        zeros = np.append(zeros, [x1,x2])
-        print ("This equation has two solutions: ", x1, " and", x2)
-    
-    return zeros
-
 points = 100
-minValues = points*-1
-maxValues = points
 
 a = 1
 b = -6
 c = 11
 d = -6
-e = 9
-f = 12
 
-func = [a,b,c,d,e,f]
+function_values = [a,b,c,d]
 
+func1 = Function(function_values)
+func_derivative = Function(func1.findDerivative())
+func_derivative_second = Function(func_derivative.findDerivative())
 
-func1 = Function(func, min, max)
-
-print(f"Original function: {showFunction(func1)}")
-func_derivative = func1.findDerivative()
-print(f"The function derivate is {showFunction(func_derivative)}")
+print(f"Original function: {func1.showFunction()}")
+print(f"The function derivate is {func_derivative.showFunction()}")
 #print(f"The function second derivate is {showFunction(func_derivative_second)}")
 
-
+plt.xlim(-10, 20)
+plt.ylim(-20, 30)
 plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
 
-plotFunction(func, True)
-plotFunction(func_derivative, False, "f'", "red")
-#plotFunction(func_derivative_second, "f''", "yellow")
-
+func1.plotFunction(True)
+func_derivative.plotFunction(False, "f'", "red")
+#plotFunction(func_derivative_second, False, "f''", "yellow")
 
 plt.show()
-
--- function_class.py
+++ function_class.py
@@ -1,34 +1,67 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+def toSuperscript(number):
+    superscripts = {
+        '0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴',
+        '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹',
+        '+': '⁺', '-': '⁻', '=': '⁼', '(': '⁽', ')': '⁾'
+    }
+    return ''.join(superscripts.get(char, char) for char in str(number))
+
+
 class Function:
-    function_coeffs = []
+    coefficients = []
+    points = 0
+    minValues = 0
+    maxValues = 0
     xpoints = np.array([])
     ypoints = np.array([])
     zerosX = set()
     
-    def __init__(self, coeffs, minValues, maxValues):
-        self.function_coeffs = coeffs
-        self.xpoints = np.arange(minValues, maxValues, 0.05)
+    def __init__(self, coeffs, points):
+        self.coefficients = coeffs
+        self.points = points
+        self.minValues = points*-1
+        self.maxValues = points
+        self.xpoints = np.arange(self.minValues, self.maxValues, 0.05)
         self.preparePlot()
     
-    def calcFunction(coeffArray, x):
+    def calcFunction(self, x):
         result = 0
-        degree = len(coeffArray) - 1
+        degree = len(self.coefficients) - 1
         
-        for coeff in coeffArray:
+        for coeff in self.coefficients:
             result += coeff * (x ** degree)
             degree -= 1
             
         return result
 
-
-    def findDerivative(coeffArray):
-        degree = len(coeffArray) - 1
+    def findZeros(a, b, c):
+        discriminant = b**2-(4*a*c) # discriminant
         
-        derivate = np.array(coeffArray)
+        zeros = np.array([])
+        if discriminant < 0:
+            print ("This equation has no real solution")
+        elif discriminant == 0:
+            x = (-b+math.sqrt(discriminant))/(2*a)
+            zeros = np.append(zeros, x)
+            print ("This equation has one solutions: ", x)
+        else:
+            x1 = ((-b)+math.sqrt(discriminant))/(2*a)
+            x2 = ((-b)-math.sqrt(discriminant))/(2*a)
+            zeros = np.append(zeros, [x1,x2])
+            print ("This equation has two solutions: ", x1, " and", x2)
+        
+        return zeros
+
+    def findDerivative(self):
+        degree = len(self.coefficients) - 1
+        
+        derivate = np.array(self.coefficients)
         index = 0
-        for coeff in coeffArray:
+        for coeff in self.coefficients:
             derivate[index] = coeff * degree
             degree -= 1
             index += 1
@@ -40,11 +73,48 @@ class Function:
     def preparePlot(self):
     
         for num in np.sort(self.xpoints):
-            newValue = self.calcFunction(self.func, num)
-            ypoints = np.append(ypoints, newValue)
+            newValue = self.calcFunction(num)
+            self.ypoints = np.append(self.ypoints, newValue)
             #print(f"x={num}, y={newValue}")
             if round(newValue) == 0: 
                 self.zerosX.add(round(num))
             
+    def plotFunction(self, showZeros, func_name_label = "f", plotColor = "blue"):
+    
+        plt.plot(self.xpoints, self.ypoints, color=plotColor, label=f"{func_name_label}(x)={self.showFunction()}")
+        plt.legend(fontsize=14)
+        
+        if showZeros:
+            print(f"The zeroes of the function are: {self.zerosX}")
+            zerosY = np.array(np.zeros(len(self.zerosX)))
+            
+            plt.plot(list(self.zerosX), zerosY, 'o', color=plotColor)
+
+
+
+            
+    def showFunction(self):
+        degree = len(self.coefficients) - 1
+        
+        text = ""
+        for coeff in self.coefficients:
+            if coeff == 0:  # Skip zero coefficients
+                degree -= 1
+                continue
+            
+            if text:
+                text += "+" if coeff > 0 else "-"
+            else:
+                text += "-" if coeff < 0 and not text else ""
+                
+            text += f"{abs(coeff)}"
+                    
+            if degree == 1: text += "x"
+            if degree > 1:
+                text += f"x{toSuperscript(degree)}"
+            
+            degree-=1
+            
+        return text
         
        
